92e513ff7776ef8aacde7cd82c4020149abd548c
vscalcione <vincenzo99.scalcione@gmail.com>
2021-07-04 19:28:35 +0200

    Update frontend (add-todo component added)

--- angular11-todo-api-frontend/src/app/components/add-todo/add-todo.component.ts
+++ angular11-todo-api-frontend/src/app/components/add-todo/add-todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo.model';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-add-todo',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
-  constructor() { }
+  date = new Date();
+
+  todo: Todo = {
+    title: '',
+    description: '',
+    publishedAt: this.date,
+    done: false
+  };
+
+  submitted = false;
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
   }
 
+  saveTodo(): void {
+    const data = {
+      title: this.todo.title,
+      description: this.todo.description
+    };
+
+    this.todoService.create(data).subscribe(response => {
+      console.log(response);
+      this.submitted = true;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  newTodo(): void {
+    this.submitted = false;
+    this.todo = {
+      title: '',
+      description: '',
+      publishedAt: this.date,
+      done: false
+    };
+  }
 }
